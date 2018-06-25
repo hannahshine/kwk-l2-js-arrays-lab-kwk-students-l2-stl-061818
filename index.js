1233da2baca2e101a12c13ee624f476873ccc832
Hannah Shine <hshine@visitationacademy.org>
2018-06-25 14:05:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 // Write your solution here!
 const drivers=['Milo','Otis','Garfield'];
-console.log(drivers.push('Ralph'));
-console.log(drivers);
+
+
+function destructivelyAppendDriver(name){
+  drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift(name);
+}
+
+function appendDriver(name){
+  const newEndDriver = [...drivers, "Broom"];
+  return newEndDriver;
+}
+
+function prependDriver(name){
+  const newFrontDriver = ["Arnold",...drivers];
+  return newFrontDriver;
+}
+
+function removeLastDriver(name){
+  const removeLast=drivers.slice(0, drivers.length-1);
+  return removeLast;
+}
+
+function removeFirstDriver(name){
+  const removeFirst=drivers.slice(-2);
+  return removeFirst;
+}
